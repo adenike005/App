eb7a16d11159fe2484e7e30da57c465c481843c6
Adenike <adeyinkaadenike992@gmail.com>
2023-11-24 11:12:29 +0100

    Changes to be committed: 	modified:   Screen/Signin.js 	modified:   package-lock.json 	modified:   package.json

--- Screen/Signin.js
+++ Screen/Signin.js
@@ -2,12 +2,13 @@ import { StyleSheet, Text, View,Image } from 'react-native'
 import React from 'react'
 import Images from '../Const/Images'
 import Color from '../Const/Color'
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const Signin = () => {
   return (
     <View style={{flex:1}}>
         <View>
-            <Image source={Images.logo}/>
+            <Image source={Images.Layer}/>
         </View>
      
     </View>
