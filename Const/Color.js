7f32f1ff5a355b2aec06b422a92dcd4dde8d422a
Adenike <adeyinkaadenike992@gmail.com>
2023-11-24 12:26:39 +0100

    addede

--- Const/Color.js
+++ Const/Color.js
@@ -1,7 +1,7 @@
 const Color = {
     primary: 'linear-gradient(180deg, #945AD8 0%, rgba(117, 58, 186, 0.91) 100%);',
     white : '#fff',
-    // dark: '#212529',
+    dark: '#000',
     secondary: '#EBE3F3',
     // white: '#ffff',
     // light: 'rgba(0,0,0,0.7)',
--- Screen/Signin.js
+++ Screen/Signin.js
@@ -1,17 +1,139 @@
-import { StyleSheet, Text, View,Image } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View,Image, TouchableOpacity, ScrollView, TextInput } from 'react-native'
+import React,{useState} from 'react'
 import Images from '../Const/Images'
 import Color from '../Const/Color'
-import Icon from 'react-native-vector-icons/FontAwesome';
+import Icon from 'react-native-vector-icons/Ionicons';
 
-const Signin = () => {
+
+const Signin = ({navigation}) => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
   return (
-    <View style={{flex:1}}>
-        <View>
+    <ScrollView style={{flex:1,}}>
+      <View> 
+        
+        <TouchableOpacity>
+        <Icon name="arrow-back" size={30} color={Color.dark}
+        onPress={() => navigation.goBack()} />
+        </TouchableOpacity>
+      </View>
+        <View style={{justifyContent:"center", alignItems:"center"}}>
             <Image source={Images.Layer}/>
+            <Text style={{fontFamily:"Black", fontSize:25, marginTop:10, marginBottom: 10}}>Create Account</Text>
+            <Text style={{fontFamily:"Thin", fontSize: 18,}}>Elevate your Project Management with us </Text>
+        </View>
+
+        <View style={{marginTop:20, marginHorizontal: "10%"}}>
+          <Text style={{marginBottom: 5}}>First Name</Text>
+          <View style={{width: "100%", height:50, backgroundColor:Color.secondary, display:"flex", flexDirection:"row",alignItems:"center", paddingHorizontal: 10, marginBottom: 10}}>
+           <TouchableOpacity><Icon name="person" size={20} color={Color.dark} /></TouchableOpacity>
+           <TextInput
+           placeholder='First name'
+           style={{
+            marginHorizontal: 10,
+            opacity: 0.5
+           }}/>
+          </View>
+
+          <Text style={{marginBottom: 5}}>Last name</Text>
+          <View style={{width: "100%", height:50, backgroundColor:Color.secondary, display:"flex", flexDirection:"row",alignItems:"center", paddingHorizontal: 10,marginBottom: 10}}>
+           <TouchableOpacity><Icon name="person" size={20} color={Color.dark} /></TouchableOpacity>
+           <TextInput
+           placeholder='Last name'
+           style={{
+            marginHorizontal: 10,
+            opacity: 0.5
+           }}/>
+          </View>
+
+          <Text style={{marginBottom: 5}}>Email</Text>
+          <View style={{width: "100%", height:50, backgroundColor:Color.secondary, display:"flex", flexDirection:"row",alignItems:"center", paddingHorizontal: 10,marginBottom: 10}}>
+           <TouchableOpacity><Icon name="mail" size={20} color={Color.dark} /></TouchableOpacity>
+           <TextInput
+           placeholder='Email'
+           style={{
+            marginHorizontal: 10,
+            opacity: 0.5
+           }}/>
+          </View>
+      <View>
+      <Text style={{ marginBottom: 5 }}>Password</Text>
+      <View
+        style={{
+          width: '100%',
+          height: 50,
+          backgroundColor: Color.secondary,
+          flexDirection: 'row',
+          alignItems: 'center',
+          paddingHorizontal: 10,
+          marginBottom: 10,
+        }}
+      >
+        <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', flex: 1 }}>
+          <View style={{alignItems:"center", display:"flex", flexDirection:"row"}}>
+            <TouchableOpacity>
+              <Icon name="lock-closed" size={20} color={Color.dark} />
+            </TouchableOpacity>
+            <TextInput
+              secureTextEntry={!showPassword}
+              placeholder="Password"
+              style={{
+                marginHorizontal: 10,
+                opacity: 0.5,
+                flex: 1,
+              }}
+            />
+          </View>
+          <View>
+            <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+              <Icon name={showPassword ? 'eye' : 'eye-off'} size={20} color="#000" />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </View>
+    <View>
+      <Text style={{ marginBottom: 5 }}>Confirm Password</Text>
+      <View
+        style={{
+          width: '100%',
+          height: 50,
+          backgroundColor: Color.secondary,
+          flexDirection: 'row',
+          alignItems: 'center',
+          paddingHorizontal: 15,
+          marginBottom: 10,
+        }}
+      >
+        <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', flex: 1 }}>
+          <View style={{alignItems:"center", display:"flex", flexDirection:"row"}}>
+            <TouchableOpacity>
+              <Icon name="lock-closed" size={20} color={Color.dark} />
+            </TouchableOpacity>
+            <TextInput
+              secureTextEntry={!showConfirmPassword}
+              onChangeText={(text) => setConfirmPassword(text)}
+              placeholder="Confirm Password"
+              style={{
+                marginHorizontal: 10,
+                opacity: 0.5,
+                flex: 1,
+              }}
+            />
+          </View>
+          <View >
+            <TouchableOpacity onPress={() => setShowConfirmPassword(!showConfirmPassword)}>
+              <Icon name={showConfirmPassword ? 'eye' : 'eye-off'} size={20} color="#000" />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </View>
         </View>
      
-    </View>
+    </ScrollView>
   )
 }
 
