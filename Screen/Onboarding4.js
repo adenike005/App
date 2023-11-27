c9f6f9ff1bd2dc7ef4ae95dab89f0180539eee6a
Adenike <adeyinkaadenike992@gmail.com>
2023-11-27 14:41:08 +0100

    added onboarding

---
+++ Screen/Onboarding4.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Onboarding4 = () => {
+  return (
+    <View>
+      <Text>Onboarding4</Text>
+    </View>
+  )
+}
+
+export default Onboarding4
